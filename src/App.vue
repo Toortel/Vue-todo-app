e187ef766f3de4eb1756a14fe6d6f885dcff81ec
Jakub Domagała <falonix01@gmail.com>
2023-07-15 20:25:31 +0200

    Added wireframe and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Vue Todo </v-list-item-title>
+          <v-list-item-subtitle>
+            The best Vuetify Todo App
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -12,18 +33,19 @@
 
     <v-main>
       <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-const drawer = ref(null);
-</script>
-
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+  }),
 };
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-8">
+    <h1>About Vuetify Todo App</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+      repudiandae illum iure! Dolorem assumenda temporibus minima dolorum
+      reprehenderit possimus, deserunt iste hic blanditiis sit eum sed
+      voluptatem, enim expedita totam!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+      repudiandae illum iure! Dolorem assumenda temporibus minima dolorum
+      reprehenderit possimus, deserunt iste hic blanditiis sit eum sed
+      voluptatem, enim expedita totam!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+      repudiandae illum iure! Dolorem assumenda temporibus minima dolorum
+      reprehenderit possimus, deserunt iste hic blanditiis sit eum sed
+      voluptatem, enim expedita totam!
+    </p>
   </div>
 </template>

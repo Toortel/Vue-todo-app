5328bf36727efd933c879fc709135e3fe43a4301
Jakub Domagała <falonix01@gmail.com>
2023-07-15 22:05:32 +0200

    Added addTask function

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    <v-list class="pt-0" flat>
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      outlined
+      label="Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+      class="pa-4"
+    ></v-text-field>
+    <v-list v-if="tasks.length" class="pt-0" flat>
       <div v-for="task in tasks" :key="task.id">
         <v-list-item
           class="py-1 px-4"
@@ -18,11 +29,21 @@
                 >{{ task.title }}</v-list-item-title
               >
             </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
       </div>
     </v-list>
+    <div v-else class="no-tasks">
+      <v-icon size="100" color="primary"> mdi-check </v-icon>
+      <div class="text-h5 primary--text">No tasks</div>
+    </div>
   </div>
 </template>
 
@@ -34,25 +55,28 @@ export default {
       const task = this.tasks.filter((task) => task.id === taskId)[0];
       task.done = !task.done;
     },
+    deleteTask(taskId) {
+      this.tasks.splice((task) => task.id === taskId, 1);
+    },
+    addTask() {
+      const nextId = this.tasks.length + 1;
+      this.tasks.push({ id: nextId, title: this.newTaskTitle, done: false });
+      this.newTaskTitle = "";
+    },
   },
   data: () => ({
-    tasks: [
-      {
-        id: 1,
-        title: "Wake up",
-        done: false,
-      },
-      {
-        id: 2,
-        title: "Buy groceries",
-        done: false,
-      },
-      {
-        id: 3,
-        title: "Make dinner",
-        done: false,
-      },
-    ],
+    newTaskTitle: "",
+    tasks: [],
   }),
 };
 </script>
+
+<style lang="scss">
+.no-tasks {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0.5;
+}
+</style>
